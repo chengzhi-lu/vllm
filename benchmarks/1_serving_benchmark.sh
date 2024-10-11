fede4f3b6b2590777732e9bb869f1384b23e4495
mchen <mchen644@gatech.edu>
2024-10-11 12:23:07 +0800

    change the shell file back for now

--- benchmarks/1_serving_benchmark.sh
+++ benchmarks/1_serving_benchmark.sh
@@ -13,8 +13,8 @@ echo $COUNTER >$COUNTER_FILE
 # start vllm server
 pwd=`pwd`
 # model_name="meta-llama/Llama-2-70b-chat-hf"
-model_name="meta-llama/Llama-2-13b-chat-hf"
-# model_name="mistralai/Mistral-7B-Instruct-v0.1" # 32000
+# model_name="meta-llama/Llama-2-13b-chat-hf"
+model_name="mistralai/Mistral-7B-Instruct-v0.1" # 32000
 # model_name="EleutherAI/gpt-neox-20b"
 # model_name="facebook/opt-6.7b"
 dataset_name="sharegpt"
@@ -26,7 +26,7 @@ result_dir="/root/v1/vllm/benchmarks/result"
 # swap_policies=(partial)
 declare -a scheduler_swap_policies
 scheduler_swap_policies[0]="tfittradeoff partial"
-scheduler_swap_policies[1]="fcfs full"
+# scheduler_swap_policies[1]="fcfs full"
 # scheduler_swap_policies[2]="las full"
 # scheduler_swap_policies[1]="tfittradeoff full"
 # scheduler_swap_policies[2]="sjf full"
@@ -37,19 +37,19 @@ scheduler_swap_policies[1]="fcfs full"
 
 preemption_mode="swap"
 gpu_memory_utilization=0.7 # 0.5, 0.7, 0.9
-max_num_seqs=256 
+max_num_seqs=384 
 swap_space=64
 max_tokens=2048
 iter_theshold=15
-max_serving_time=60
+
 # request_rates[0]=0.5
 # request_rates[1]=1.0
 # request_rates[2]=2.0
 # request_rates[3]=5.0
 # request_rates[4]=10.0
-request_rates[5]=20.0
+# request_rates[5]=20.0
 # request_rates[5]=30.0
-# request_rates[5]=50.0
+request_rates[5]=50.0
 # request_rates[5]=100.0
 
 # request_rates=(2.0)
@@ -66,16 +66,16 @@ for i in {0..0}; do
           swap_policy=${element[1]}
           # tmux new-session -s "api_server" -d bash start_server.sh $gpu_devices $model_name $swap_space $preemption_mode $policy $max_tokens $iter_theshold $max_num_seqs $swap_policy $swap_out_partial_rate $gpu_memory_utilization $waiting_iter
 
-          CUDA_VISIBLE_DEVICES=$gpu_devices taskset -c 23-24 python3 -m vllm.entrypoints.openai.api_server \
+          CUDA_VISIBLE_DEVICES=$gpu_devices taskset -c 20-21 python3 -m vllm.entrypoints.openai.api_server \
             --model $model_name --swap-space $swap_space --preemption-mode $preemption_mode --scheduler-policy $policy \
             --enable-chunked-prefill --max-num-batched-tokens $max_tokens --iter-threshold $iter_theshold --max-num-seqs $max_num_seqs --swap-out-tokens-policy $swap_policy --swap-out-partial-rate $swap_out_partial_rate --execution-budget $iter_theshold \
-            --tensor-parallel-size 1 --gpu-memory-utilization $gpu_memory_utilization --disable-sliding-window --waiting-iter-base $waiting_iter --disable-log-requests --max-serving-time $max_serving_time >api_server_${policy}_${swap_policy}.log 2>&1 &
+            --tensor-parallel-size 1 --gpu-memory-utilization $gpu_memory_utilization --disable-sliding-window --waiting-iter-base $waiting_iter --disable-log-requests >api_server_${policy}_${swap_policy}.log 2>&1 &
           pid=$! 
 
           # run benchmark and save the output to benchmark.log
           python3 benchmark_serving.py --execution-counter $COUNTER --dataset-path $dataset_path \
             --dataset-name $dataset_name --request-rate $request_rate \
-            --num-prompts 500 --request-duration $max_serving_time --sharegpt-output-len 2000 --model $model_name --scheduler-policy $policy \
+            --num-prompts 500 --request-duration 500 --sharegpt-output-len 2000 --model $model_name --scheduler-policy $policy \
             --save-result --result-dir $result_dir \
             --metadata swap_space=$swap_space preemption_mode=$preemption_mode \
             scheduler_policy=$policy gpu_memory_utilization=$gpu_memory_utilization \
